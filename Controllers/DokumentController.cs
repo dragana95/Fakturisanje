0a2f234f04d7e17fdbdcd05d9910fb1e0d470270
DESKTOP-6VRO6F2\draga <dragana.dimitrijevic@openpuzzles.com>
2019-02-03 02:24:04 +0100

    Initial commit

---
+++ Controllers/DokumentController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using Fakturisanje.Models;
+
+namespace Fakturisanje.Controllers
+{
+    public class DokumentController : Controller
+    {
+        // GET: Dokument Dokument/Index
+        public ActionResult Index()
+        {
+            using (LocalDBEntities dbEntity = new LocalDBEntities())
+            {
+                return View(dbEntity.DokumentFaktures.ToList());
+            }
+        }
+
+        // GET: Dokument/Details/5
+        public ActionResult Details(string id)
+        {
+            using (LocalDBEntities dbmodel = new LocalDBEntities())
+            {
+                var rezultat = new Faktura
+                {
+                    
+                    FK_DokumetFakture = dbmodel.DokumentFaktures.FirstOrDefault(x => x.RedniBroj.Equals(id)).RedniBroj,
+                    DatumFakture = dbmodel.DokumentFaktures.FirstOrDefault(x => x.RedniBroj.Equals(id)).DatumFakture,
+                    BrojFakture = dbmodel.DokumentFaktures.FirstOrDefault(x => x.RedniBroj.Equals(id)).RedniBroj,
+
+                    S = (from s in dbmodel.StavkaFaktures join d in dbmodel.DokumentFaktures
+                         on s.FK_DokumentFakture equals d.RedniBroj
+                         where s.FK_DokumentFakture.Equals(id)
+                         select new { s.RedniBroj, s.Kolicina, s.Cena, s.Ukupno })
+                         .ToDictionary(k => k.RedniBroj, v => Tuple.Create(v.Kolicina, v.Cena, v.Ukupno))
+                };
+
+                return View(rezultat);
+
+            }
+        }
+
+        // GET: Dokument/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dokument/Create
+        [HttpPost]
+        public ActionResult Create(DokumentFakture dokument)
+        {
+                // TODO: Add insert logic here
+            try
+            {
+                    using (LocalDBEntities dBEntity = new LocalDBEntities())
+                    {
+                        dBEntity.DokumentFaktures.Add(dokument);
+                        dBEntity.SaveChanges();
+                    }
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("Faktura nije dodata!");
+                return View();
+            }
+        }
+
+        // GET: Dokument/Edit
+        public ActionResult Edit(string id)
+        {
+            using (LocalDBEntities dBEntity = new LocalDBEntities())
+            {
+                return View(dBEntity.DokumentFaktures.Where(x => x.RedniBroj == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Dokument/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, DokumentFakture dokument)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using(LocalDBEntities dbEntity = new LocalDBEntities())
+                {
+                    string idStr = id.ToString();
+                    dbEntity.Entry(dokument).State = System.Data.EntityState.Modified;
+                    dbEntity.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Dokument/Delete/5
+        public ActionResult Delete(string id)
+        {
+            using (LocalDBEntities dBEntity = new LocalDBEntities())
+            {
+                return View(dBEntity.DokumentFaktures.Where(x => x.RedniBroj == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Dokument/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using(LocalDBEntities dbEntity = new LocalDBEntities())
+                {
+                    DokumentFakture dokument = dbEntity.DokumentFaktures.Where(x => x.RedniBroj == id).FirstOrDefault();
+                    dbEntity.DokumentFaktures.Remove(dokument);
+                    dbEntity.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Faktura.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fakturisanje.Models
+{
+    public class Faktura
+    {
+        [DisplayName("Redni broj")]
+        public int RedniBroj { get; set; }
+
+        public string FK_DokumetFakture { get; set; }
+
+        public Dictionary<int, Tuple<int, double, double>> S { get; set; }
+
+        [DisplayName("Datum fakture")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public System.DateTime DatumFakture { get; set; }
+
+        [DisplayName("Broj fakture")]
+        public string BrojFakture { get; set; }
+
+        //public Faktura S { get; set; }
+    }
+}
